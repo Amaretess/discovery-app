a2710355b337fc500d7140b1112271154424370d
Ashlyn Barna <ashlyn.barna@icloud.com>
2024-01-28 23:17:48 -0800

    errors

--- src/App.tsx
+++ src/App.tsx
@@ -10,15 +10,18 @@ const App = () => {
 
 
   const [users, setUsers] = useState<User[]>([]);
+  const [error, setError] = useState('');
 
   useEffect(() => {
     // <User[]> tells axios what kind of data we are using
-    axios.get<User[]>('https://jsonplaceholder.typicode.com/users')
+    axios.get<User[]>('https://jsonplaceholder.typicode.com/xusers')
       .then(res => setUsers(res.data))
+      .catch(err => setError(err.message))
   }, [])
 
   return (
     <ul>
+      {error && <p className="text-danger" >{error}</p>}
       {users.map(user => <li key={user.id}>{user.name}</li>)}
 
     </ul>
