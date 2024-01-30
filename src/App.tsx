d302f89eb7613e1087bf534245b3fdefa90abd8d
Ashlyn Barna <ashlyn.barna@icloud.com>
2024-01-29 23:52:01 -0800

    memorized

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,19 @@
-import axios from 'axios';
-import { useEffect, useState } from 'react'
-
-interface User {
-  id: number;
-  name: string;
-}
+import axios from "axios";
+import { useEffect, useState } from "react"
 
 const App = () => {
 
-  const [users, setUsers] = useState<User[]>([]);
+  const [users, setUsers] = useState([]);
   const [err, setErr] = useState('');
-  const [isLoading, setLoading] = useState(false);
-
 
   useEffect(() => {
-
-    const controller = new AbortController();
-
-    setLoading(true)
-
-    axios.get<User[]>('https://jsonplaceholder.typicode.com/users')
-      .then(res => {
-        setUsers(res.data);
-        setLoading(false)
-      })
-      .catch(err => setErr(err.message));
-
-    return () => controller.abort();
+    axios.get()
   }, [])
 
   return (
-    <>
-      {err && <p className="text-danger">{err}</p>}
-      {isLoading && <div className="spinner-border"></div>}
-      <ul>
-        {users.map((user) => (
-          <li key={user.id}>{user.name}</li>
-        ))}
-      </ul>
-    </>
+    <div>
+
+    </div>
   )
 }
 
