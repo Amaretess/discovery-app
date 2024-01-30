e441e934ecd38b880b203457a07a0a995e263572
Ashlyn Barna <ashlyn.barna@icloud.com>
2024-01-29 23:55:20 -0800

    err

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,28 @@
 import axios from "axios";
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
+
+interface User {
+  id: number;
+  name: string;
+}
 
 const App = () => {
 
-  const [users, setUsers] = useState([]);
+  const [users, setUsers] = useState<User[]>([]);
   const [err, setErr] = useState('');
 
   useEffect(() => {
-    axios.get()
+    axios.get<User[]>('https://jsonplaceholder.typicode.com/users')
+      .then(res => setUsers(res.data))
+      .catch(err => setErr(err.message));
   }, [])
 
   return (
-    <div>
-
-    </div>
+    <>
+      <ul>
+        {users.map(user => <li key={user.id} >{user.name}</li>)}
+      </ul>
+    </>
   )
 }
 
