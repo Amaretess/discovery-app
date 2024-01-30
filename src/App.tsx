9a3b61f41fb72a74859c50d814821afe885f068e
Ashlyn Barna <ashlyn.barna@icloud.com>
2024-01-30 00:01:16 -0800

    loading

--- src/App.tsx
+++ src/App.tsx
@@ -10,15 +10,23 @@ const App = () => {
 
   const [users, setUsers] = useState<User[]>([]);
   const [err, setErr] = useState('');
+  const [isLoading, setLoading] = useState(false)
 
   useEffect(() => {
+
+    setLoading(true)
+
     axios.get<User[]>('https://jsonplaceholder.typicode.com/users')
-      .then(res => setUsers(res.data))
+      .then(res => {
+        setUsers(res.data)
+        setLoading(false);
+      })
       .catch(err => setErr(err.message));
   }, [])
 
   return (
     <>
+      {isLoading && <p className="text-danger"></p>}
       {err && <p className="text-danger" >{err}</p>}
       <ul>
         {users.map(user => <li key={user.id} >{user.name}</li>)}
