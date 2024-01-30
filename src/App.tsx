e2ef0e84916395d6198c095dc9767503db9be4df
Ashlyn Barna <ashlyn.barna@icloud.com>
2024-01-29 17:09:47 -0800

    loading

--- src/App.tsx
+++ src/App.tsx
@@ -10,15 +10,20 @@ const App = () => {
 
   const [users, setUsers] = useState<User[]>([]);
   const [err, setErr] = useState('');
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setLoading] = useState(false);
 
 
   useEffect(() => {
 
     const controller = new AbortController();
 
+    setLoading(true)
+
     axios.get<User[]>('https://jsonplaceholder.typicode.com/users')
-      .then(res => setUsers(res.data))
+      .then(res => {
+        setUsers(res.data);
+        setLoading(false)
+      })
       .catch(err => setErr(err.message));
 
     return () => controller.abort();
@@ -27,6 +32,7 @@ const App = () => {
   return (
     <>
       {err && <p className="text-danger">{err}</p>}
+      {isLoading && <div className="spinner-border"></div>}
       <ul>
         {users.map((user) => (
           <li key={user.id}>{user.name}</li>
