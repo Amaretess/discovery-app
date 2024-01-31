e1030b4a203913f8e9efc594aad50c8287d93e8b
Ashlyn Barna <ashlyn.barna@icloud.com>
2024-01-30 20:33:34 -0800

    dependency err

--- src/App.tsx
+++ src/App.tsx
@@ -34,15 +34,33 @@ const App = () => {
   }, []);
 
   const deleteUser = (user: User) => {
+    const originalUsers = [...users]
     setUsers(users.filter(u => u.id !== user.id))
 
+    axios.delete('https://jsonplaceholder.typicode.com/users/' + user.id)
+      .catch(err => {
+        setErr(err.message)
+        setUsers(originalUsers);
+      })
   }
 
+  const addUser = () => {
+    const originalUsers = [...users];
+    const newUser = { id: 1, name: 'Ash' };
+    setUsers([newUser, ...users]);
+
+    axios.post('https://jsonplaceholder.typicode.com/users/' + newUser)
+      .catch(err => {
+        setErr(err);
+        setUsers(originalUsers)
+      })
+  };
 
   return (
     <>
       {isLoading && <div className="spinner-border"></div>}
       {err && <p className="text-danger" >{err}</p>}
+      <button onClick={() => addUser()} className="btn btn-outline-primary" >Add</button>
       <ul className="list-group" >
         {users.map((user) => (
           <li className="list-group-item d-flex justify-content-between" key={user.id} >
