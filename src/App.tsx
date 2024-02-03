cc112b467437c9ad9d5a213067d85beec781ca51
Ashlyn Barna <ashlyn.barna@icloud.com>
2024-02-02 23:46:51 -0800

    errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,43 @@
 import axios from 'axios';
 import apiClient from './services/api-client';
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
+
+interface User {
+  id: number;
+  name: string;
+}
 
 const App = () => {
 
-  const [users, setUsers] = useState([]);
+  const [users, setUsers] = useState<User[]>([]);
   const [error, setError] = useState('');
   const [isLoading, setLoading] = useState(false);
 
 
   useEffect(() => {
+
+    const controller = new AbortController();
+
+    setLoading(true);
+
     apiClient
       .get('/users')
-      .then(({ data: savedUser }) => {
-        setUsers(savedUser)
+      .then(({ data: allUsers }) => {
+        setUsers(allUsers);
+        setLoading(false);
       })
+      .catch((err) => {
+        setError(err.message)
+      })
+    controller.abort();
   })
 
   return (
     <>
-      <ul>
-
+      {isLoading && <div className='spinner-border' ></div>}
+      {error && <p className='text-danger' >{error}</p>}
+      <ul className="list-group" >
+        {users.map((user) => <li className='list-group-item' key={user.id} >{user.name}</li>)}
       </ul>
     </>
   )
