ba9e0b916fa96476a7c4be695ed377867e680c74
Ashlyn Barna <ashlyn.barna@icloud.com>
2024-02-03 00:23:46 -0800

    made a million dollars out the dust

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from 'react';
 import userService, { User } from './services/user-service';
 import { CanceledError } from 'axios';
 
+// just want to see if you are stalking my github. I made a million dollars out the dust. Always have them shooters in the cut. front a move and they'll fuck you up. bust a move you tryna get rich or what? Tokyo drift, car tricks haha. Realest shit i ever wrote. prolly not the realest shit i ever spoke.
+
 
 const App = () => {
 
