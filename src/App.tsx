d5a1bcafd1c4fc5dc18f5de7ba943f8f5ad7411c
Ashlyn Barna <ashlyn.barna@icloud.com>
2024-02-02 23:27:36 -0800

    starting over for good measure

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,10 @@
-import { CanceledError } from "axios";
-import userService from "./services/user-service";
-import { useEffect, useState } from "react";
-import { User } from "./services/user-service";
-
+import React from 'react'
 
 const App = () => {
-
-  const [users, setUsers] = useState<User[]>([]);
-  const [error, setError] = useState('');
-  const [isLoading, setLoading] = useState(false);
-
-
-  useEffect(() => {
-
-    setLoading(true);
-
-    const { cancel, request } = userService.getAllUsers()
-    request.then(res => {
-      setUsers(res.data)
-      setLoading(false);
-    })
-      .catch(err => {
-        if (err instanceof CanceledError) return;
-        setError(err.message)
-        setLoading(false);
-      });
-
-    return () => cancel();
-  }, [])
-
   return (
-    <>
-      {isLoading && <div className="spinner-border"></div>}
-      {error && <p className="danger-text"></p>}
-      <ul>
-        {users.map(user => <li key={user.id} >{user.name}</li>)}
-      </ul>
-    </>
+    <div>
+
+    </div>
   )
 }
 
