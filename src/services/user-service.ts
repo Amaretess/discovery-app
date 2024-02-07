802c1117e31ea94c9508a286fee3645f284196e1
Ashlyn Barna <ashlyn.barna@icloud.com>
2024-02-07 01:32:40 -0800

    mastering api client

--- src/services/user-service.ts
+++ src/services/user-service.ts
@@ -1,3 +1,13 @@
-class userService {
-    
+import axios from "axios";
+
+
+
+class UserService {
+    getAllUsers() {
+        const response = axios.get('https://jsonplaceholder.typicode.com/users');
+        const cancel = new AbortController();
+        return { response, cancel: cancel.abort() } 
+    }
 }
+
+export default new UserService();
