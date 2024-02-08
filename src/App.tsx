6d9aa959807af035cf33731beb78e4dfc607cba9
Ashlyn Barna <ashlyn.barna@icloud.com>
2024-02-07 23:59:25 -0800

    done in 10

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,24 @@
 import { useEffect, useState } from "react";
-import commentService from "./services/comment-service";
-import { Comment } from "./services/comment-service";
-import { CanceledError } from './services/api-client';
-
+import { User } from "./services/user-service";
+import userService from "./services/user-service";
 
 const App = () => {
 
-  const [comments, setComments] = useState<Comment[]>([]);
+  const [users, setUsers] = useState<User[]>([]);
   const [error, setError] = useState('');
   const [isLoading, setLoading] = useState(false);
 
   useEffect(() => {
-    setLoading(true);
-    const { request, cancel } = commentService.getAllComments()
-    request.then(({ data: allComments }) => {
-      setComments(allComments)
-      setLoading(false)
-    })
-      .catch((err) => {
-        if (err instanceof CanceledError) return;
-        setError(err.message);
-        setLoading(false);
+    userService.getAllUsers()
+      .request.then(({ data: allUsers }) => {
+        setUsers(allUsers);
       })
-
-    return cancel()
   }, [])
 
   return (
-
-    <>
-      {isLoading && <div className="spinner-border"></div>}
-      {error && <p >{error}</p>}
-      <ul>
-        {comments.map((comment) => (
-          <li key={comment.id} >
-            {comment.name}
-          </li>
-        ))}
-
-      </ul>
-    </>
+    <ul>
+      {users.map((user) => <li key={user.id}>{user.name}</li>)};
+    </ul>
   )
 }
 
