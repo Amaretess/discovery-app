21448210b7881af9ee721be8dbc052f3644219fe
Ashlyn Barna <ashlyn.barna@icloud.com>
2024-02-07 17:30:41 -0800

    attachments

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,12 @@
-import { useEffect, useState } from "react";
-import userService from "./services/user-service";
-import { User } from './services/user-service';
-
 
 
 const App = () => {
 
-  const [users, setUsers] = useState<User[]>([]);
-  const [error, setError] = useState('');
-  const [isLoading, setLoading] = useState(false);
-
-  useEffect(() => {
-    setLoading(true);
-    userService.getAllUsers()
-      .response.then(({ data: allUsers }) => {
-        setUsers(allUsers)
-        setLoading(false);
-      })
-      .catch((err) => {
-        setError(err.message)
-      })
-  }, [])
-
 
   return (
-    <>
-      {isLoading && <div className="spinner-border"></div>}
-      {error && <p className="text-danger">{error}</p>}
-      { }
-      <ul>
-        {users.map((user) => (
-          <li key={user.id} >
-            {user.name}
-          </li>
-        ))}
-      </ul>
-    </>
+    <div>
+
+    </div>
   )
 }
 
