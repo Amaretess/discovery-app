eafaaf7e3a1dccb1282825fc588ef0d55ded8145
Ashlyn Barna <ashlyn.barna@icloud.com>
2024-02-11 19:36:36 -0800

    love you

--- src/App.tsx
+++ src/App.tsx
@@ -1,59 +1,37 @@
-// i hate you so much
-
-import { useState, useEffect } from 'react'
-import apiClient, { CanceledError } from './services/api-client';
-
-interface User {
-  id: number;
-  name: string;
-}
+import { useEffect, useState } from 'react'
+import userService, { User } from './services/user-service';
+import { CanceledError } from './services/api-client';
 
 const App = () => {
 
-  const [users, setUsers] = useState<User[]>([])
+  const [users, setUsers] = useState<User[]>([]);
   const [error, setError] = useState('');
   const [isLoading, setLoading] = useState(false);
 
+
   useEffect(() => {
-    setLoading(false);
-
-    const controller = new AbortController();
-
-    apiClient.get('/users')
-      .then(({ data: allUsers }) => {
-        setUsers(allUsers)
-        setLoading(false);
-      })
-      .catch(err => {
+    setLoading(true);
+    const { request, cancel } = userService.getAllUsers();
+    request.then(({ data: allUsers }) => {
+      setUsers(allUsers)
+      setLoading(false);
+    })
+      .catch((err) => {
         if (err instanceof CanceledError) return;
-        setError(err.message)
+        setError(err.message);
         setLoading(false);
       })
-
-    return () => controller.abort();
+    return () => cancel();
   }, [])
 
-  const updateUser = (user: User) => {
-    const updatedUser = [{ ...user, name: user.name + '!' }]
-    setUsers(users.filter(u => u.id === user.id ? updatedUser : user));
-  }
-
-  const deleteUser = (user: User) => {
-    setUsers(users.filter(u => u.id !== user.id))
-  }
-
   return (
+
     <>
-      {isLoading && <div className="spinner-border"></div>}
       {error && <p className="text-danger">{error}</p>}
-      <ul className='list-group'>
-        {users.map(user => <li className='list-group-item d-flex justify-content-between' key={user.id} >
-          {user.name}
-          <div>
-            <button onClick={() => updateUser(user)} className='btn btn-outline-secondary' >Update</button>
-            <button onClick={() => deleteUser(user)} className='btn btn-outline-danger' >Delete</button>
-          </div>
-        </li>)}
+      {isLoading && <div className="spinner-border"></div>}
+      <ul>
+        {users.map((user) => <li key={user.id} >{user.name}</li>)}
+
       </ul>
     </>
   )
