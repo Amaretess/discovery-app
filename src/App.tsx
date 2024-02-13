ee7fd86ea2a6c38c41709d27776e7497207bceb4
Ashlyn Barna <ashlyn.barna@icloud.com>
2024-02-12 23:48:36 -0800

    hook

--- src/App.tsx
+++ src/App.tsx
@@ -25,15 +25,27 @@ const App = () => {
     return () => cancel()
   }, [])
 
+  const updateUser = () => {
+
+  }
+
+  const deleteUser = () => {
+
+  }
+
   return (
 
     <>
       {error && <p className="text-danger">{error}</p>}
       {isLoading && <div className="spinner-border"></div>}
       <button className="btn btn-primary m-1" >Add User</button>
-      <ul>
-        {users.map((user) => <li>
-          <li>{user.name}</li>
+      <ul className="list-group">
+        {users.map((user) => <li className="list-group-item d-flex justify-content-betweeen" key={user.id}>
+          {user.name}
+          <div>
+            <button onClick={() => updateUser()} className="btn btn-outline-secondary" >Update</button>
+            <button onClick={() => deleteUser()} className="btn btn-outline-secondary" >Delete</button>
+          </div>
         </li>)}
 
       </ul>
