d9362f12a4fe1b169ea485bd5e18e27d93d29584
Ashlyn Barna <ashlyn.barna@icloud.com>
2024-02-13 13:18:52 -0800

    practicing http-services

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,23 @@
 // it's always the same, "my ex taught me"
 
 import useUsers from "./hooks/useUsers";
+import userService, { User } from "./services/user-service";
 
 const App = () => {
 
   const { users, error, isLoading, setUsers, setError } = useUsers();
 
   // these funcs specific to component
-  const updateUser = () => {
+  const updateUser = (user: User) => {
+    const updatedUser = { ...user, id: 0, name: user.name + '!' }
+    setUsers(users.map((u) => u.id === user.id ? updatedUser : u))
+    userService.update(user)
 
   }
 
-  const deleteUser = () => {
-
+  const deleteUser = (id: number) => {
+    setUsers(users.filter((u) => u.id !== id))
+    userService.delete(id)
   }
 
   return (
@@ -22,11 +27,11 @@ const App = () => {
       {isLoading && <div className="spinner-border"></div>}
       <button className="btn btn-primary m-1" >Add User</button>
       <ul className="list-group">
-        {users.map((user) => <li className="list-group-item d-flex justify-content-betweeen" key={user.id}>
+        {users.map((user) => <li className="list-group-item d-flex justify-content-between" key={user.id}>
           {user.name}
           <div>
-            <button onClick={() => updateUser()} className="btn btn-outline-secondary" >Update</button>
-            <button onClick={() => deleteUser()} className="btn btn-outline-secondary" >Delete</button>
+            <button onClick={() => updateUser(user)} className="btn btn-outline-secondary mx-1" >Update</button>
+            <button onClick={() => deleteUser(user.id)} className="btn btn-outline-danger" >Delete</button>
           </div>
         </li>)}
 
