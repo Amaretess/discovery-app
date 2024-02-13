638c49f9ebda00520d812e738200316a302549e0
Ashlyn Barna <ashlyn.barna@icloud.com>
2024-02-13 00:07:43 -0800

    custom hook

--- src/App.tsx
+++ src/App.tsx
@@ -6,25 +6,8 @@ import { CanceledError } from "./services/api-client";
 
 const App = () => {
 
-  const [users, setUsers] = useState<User[]>([]);
-  const [error, setError] = useState('');
-  const [isLoading, setLoading] = useState(false);
 
-  useEffect(() => {
-    setLoading(true);
-    const { request, cancel } = userService.getAll<User>()
-    request.then(({ data: allUsers }) => {
-      setUsers(allUsers);
-      setLoading(false);
-    })
-      .catch(err => {
-        if (err instanceof CanceledError) return;
-        setError(err.message)
-        setLoading(false)
-      })
-    return () => cancel()
-  }, [])
-
+  // these funcs specific to component
   const updateUser = () => {
 
   }
